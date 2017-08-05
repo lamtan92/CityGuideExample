a4cf400d7c1480be3f4e955e122bf43429a3b681
Tran Tan Lam <trankind@gmail.com>
2017-08-05 17:05:04 +0700

    Initial Commit

---
+++ CityGuideExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CityGuideExample
+//
+//  Created by Lam Tran on 8/5/17.
+//  Copyright © 2017 Tan Lam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
